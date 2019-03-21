e9fa55fd20fb48dd6517cca0c00fa49cea8b635d
Joy Lin <vvlnote@hotmail.com>
2019-03-21 22:16:06 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
+  def initialize(book_title)
+    @title = book_title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
 
